a42e919d94c3372b02fab240d8ad79c9c5b37352
Prajwal Masulkar <54013334+PrajwalMasulkar@users.noreply.github.com>
2022-06-05 01:23:18 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'jfeivr6wgxuw__9bxx^jxsv@u&mxdss+j$y-9ityd8&a#q6hrx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sentiment-analysis-logistic.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['sentiment-analysis-logistic.herokuapp.com/polls', '127.0.0.1']
 
 
 # Application definition
